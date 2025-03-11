f8ec7e97aa14050ff40cc488788af89f7e3e206e
호봉쓰 <hobongs@hobongsseuui-MacBookPro.local>
2025-03-11 23:08:34 +0900

    day19 first commit

---
+++ day19/main.py
@@ -0,0 +1,25 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+def move_forward():
+    tim.forward(10)
+
+def move_backward():
+    tim.forward(-10)
+
+def move_left():
+    tim.left(10)
+
+def move_right():
+    tim.right(10)
+
+screen.listen()
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="s", fun=move_backward)
+screen.onkey(key="d", fun=move_right)
+screen.onkey(key="a", fun=move_left)
+
+
+screen.exitonclick()
