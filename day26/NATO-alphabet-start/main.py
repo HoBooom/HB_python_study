fdcfbfab3b5b1552176507195c6e6a17fe4dc1c9
호봉쓰 <twomam0376@naver.com>
2025-03-22 23:15:48 +0900

    day26clear list comprehension, nato alpha

---
+++ day26/NATO-alphabet-start/main.py
@@ -0,0 +1,38 @@
+# student_dict = {
+#     "student": ["Angela", "James", "Lily"],
+#     "score": [56, 76, 98]
+# }
+#
+# #Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     #Access key and value
+#     pass
+#
+# import pandas
+# student_data_frame = pandas.DataFrame(student_dict)
+#
+# #Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     #Access index and row
+#     #Access row.student or row.score
+#     pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+#TODO 1. Create a dictionary in this format:
+import pandas
+
+nato_data = pandas.read_csv('nato_phonetic_alphabet.csv')
+# for (index,row) in nato_data.iterrows():
+#     print("index : ",index)
+#     print("row : \n",row)
+nato_data_dict = {row.letter:row.code for (index,row) in nato_data.iterrows()}
+print(nato_data_dict)
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word = input('Enter a word: ')
+# print([alpha for alpha in word])
+new_nato = [nato_data_dict[alpha.upper()] for alpha in word]
+print(new_nato)
+
---
+++ day26/main.py
@@ -0,0 +1,6 @@
+# nums = [x for x in range(1,5)]
+# nums1 = [2*x for x in nums]
+# print(*nums)
+# print(*nums1)
+# missing_states = [state for state in all_states if state not in guessed_states]
+
